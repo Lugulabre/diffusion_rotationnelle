9a2877de85e4ef429c0e7483233f9a665e87dce7
apo23 <47241570+apo23@users.noreply.github.com>
2020-02-25 22:56:35 +0100

    Delete barycentre.py

--- barycentre.py
+++
@@ -1,9 +0,0 @@
-def barycentre(x_dict, y_dict, z_dict):
-    barycentres = {}
-    for i in range(0, len(x_dict)):
-        temp = 't' + str(i)
-        x_mean = (sum(x_dict[temp].values())/len(x_dict[temp]))
-        y_mean = (sum(y_dict[temp].values())/len(y_dict[temp]))
-        z_mean = (sum(z_dict[temp].values())/len(z_dict[temp]))
-        barycentres[temp] = {'x':x_mean, 'y':y_mean, 'z':z_mean}
-    return barycentres
